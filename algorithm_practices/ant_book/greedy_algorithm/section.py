78c72078dfe15ba1caee7941e9b92de8f4e93a83
oyu <oyuwantto@gmail.com>
2019-12-01 17:45:00 +0900

    itvって何の略なん

--- algorithm_practices/ant_book/greedy_algorithm/section.py
+++ algorithm_practices/ant_book/greedy_algorithm/section.py
@@ -1,4 +1,5 @@
-'''     p. 43
+'''
+p. 43
 貪欲法 (Greedy algorithm) 
 
 -区間の問題-
@@ -9,8 +10,6 @@ n個の仕事があります。各仕事は時間s_iに始まり、時間t_iに�
 
 できるだけ多くの仕事に参加したいです。何個の仕事に参加することができるでしょうか。
 
-※テキストには図もあります
-
 -制約-
 ・1 <= N <= 100000
 ・1 <= s_i <= t_i <= 10^9
@@ -19,20 +18,37 @@ n個の仕事があります。各仕事は時間s_iに始まり、時間t_iに�
 入力
 N = 5, s = {1, 2, 4, 6, 8}, t = {3, 5, 7, 9, 10}
 
+5
+1 3
+2 5
+4 7
+6 9
+8 10
+
 出力
 3
 '''
 
 import numpy as np
 
-max_N = 100000
-
 # 入力
 N = int(input())
 
 itv = []
-for i in range(N):
-
+range_N = range(N)
+for i in range_N:
     s, t = map(int, input().split())
-    itv.append([s, t])
+    itv.append([t, s])
 
+# 2次元配列を辞書順でソート
+itv = sorted(itv, key=lambda x:(x[0], x[1]))
+
+ans = 0
+t = 0
+
+for i in range_N:
+    if t < itv[i][1]:
+        ans += 1
+        t = itv[i][0]
+
+print(ans) 
